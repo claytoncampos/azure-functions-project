6d1d57d5250558554ca8b8dab3844c9d6dfb35ab
Clayton Campos <clayton.almeida.campos@gmail.com>
2021-06-04 10:45:40 -0300

    create function DeleteProduct and create tratative error

--- CreateProduct/index.js
+++ CreateProduct/index.js
@@ -1,16 +1,30 @@
 const createMongoClient = require('../shared/mongoClient');
 
 module.exports = async function (context, req) {
-  const product = req.body;
+  const product = req.body || {};
+
+  if (product) {
+    context.res = {
+      status: 400,
+      body: 'Product is required',
+    };
+  }
 
   const { client: MongoClient, closeConnectionFn } = await createMongoClient();
   const Products = MongoClient.collection('products');
 
-  const res = await Products.insert(product);
+  try {
+    const res = await Products.insert(product);
 
-  closeConnectionFn();
-  context.res = {
-    status: 201,
-    body: res,
-  };
+    closeConnectionFn();
+    context.res = {
+      status: 201,
+      body: res.ops[0],
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: 'Error on insert product',
+    };
+  }
 };
--- DeleteProduct/index.js
+++ DeleteProduct/index.js
@@ -1,6 +1,5 @@
-const createMongoClient = require('../shared/mongoClient');
-
 const { ObjectID } = require('mongodb');
+const createMongoClient = require('../shared/mongoClient');
 
 module.exports = async function (context, req) {
   const { id } = req.params;
@@ -15,8 +14,18 @@ module.exports = async function (context, req) {
 
   const { client: MongoClient, closeConnectionFn } = await createMongoClient();
   const Products = MongoClient.collection('products');
-  const body = await Products.findOne({ _id: ObjectID(id) });
 
-  closeConnectionFn();
-  context.res = { status: 200, body };
+  try {
+    await Products.findOneAndDelete({ _id: ObjectID(id) });
+    closeConnectionFn();
+    context.res = {
+      status: 200,
+      body: 'Product deleted successfully!',
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: 'Error on delete product ' + id,
+    };
+  }
 };
