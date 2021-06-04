14e60ea17e971257f9f04d4888b0a048349ddd21
Clayton Campos <clayton.almeida.campos@gmail.com>
2021-06-04 10:28:01 -0300

    create function UpdateProduct

--- UpdateProduct/index.js
+++ UpdateProduct/index.js
@@ -1,16 +1,18 @@
 const createMongoClient = require('../shared/mongoClient');
-
 const { ObjectID } = require('mongodb');
 
 module.exports = async function (context, req) {
+  const product = req.body;
   const { id } = req.params;
 
   const { client: MongoClient, closeConnectionFn } = await createMongoClient();
   const Products = MongoClient.collection('products');
-  const res = await Products.findOne({ _id: ObjectID(id) });
+  const res = await Products.findOneAndUpdate(
+    { _id: ObjectID(id) },
+    { $set: product }
+  );
 
   closeConnectionFn();
-
   context.res = {
     status: 200,
     body: res,
